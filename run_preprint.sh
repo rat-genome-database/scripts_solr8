0b2489954e2e92e92fe3481f0dd5164d921e9332
RGD Web <rgdpub@garak.rgd.mcw.edu>
2025-09-15 13:18:50 -0500

    updated

--- run_preprint.sh
+++ run_preprint.sh
@@ -1,2 +1,4 @@
 SCRIPT_DIR=/home/rgdpub/Scripts/scripts_solr8
-$SCRIPT_DIR/run_indexer.sh preprint_output preprintSolr
+$SCRIPT_DIR/aws/generate_preprint_output.sh
+echo "Preprint JSON output generated"
+$SCRIPT_DIR/run_indexer.sh preprint_output preprintSolr preprint
