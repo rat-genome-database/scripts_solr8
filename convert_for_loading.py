ca2744dd6cce1c6f1a530e2e87ae89c0d51a80e8
RGD Web <rgdpub@garak.rgd.mcw.edu>
2025-09-15 11:08:24 -0500

    updated

--- convert_for_loading.py
+++ convert_for_loading.py
@@ -7,7 +7,7 @@ with open(sys.argv[1]) as f:
 	lines = f.readlines()
 
 for line in lines:
-        line = re.sub(r'("p_date":\["\d{4}-\d{2}-\d{2})"', '\\1T06:00:00Z"],"p_source":["preprint"', line)
+        line = re.sub(r'("p_date":\["\d{4}-\d{2}-\d{2})"', '\\1T06:00:00Z"],"p_source":["pubmed"', line)
         print(line);
         print(",")
 
