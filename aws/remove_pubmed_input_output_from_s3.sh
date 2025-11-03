29d74043faa13393eda6fa9cbf8d07a8a4e163a6
RGD Web <rgdpub@garak.rgd.mcw.edu>
2025-11-03 11:13:34 -0600

    updated scripts

---
+++ aws/remove_pubmed_input_output_from_s3.sh
@@ -0,0 +1,3 @@
+aws s3 rm s3://emr-repository/pubmed/ --recursive
+aws s3 rm s3://emr-repository/output/ --recursive
+
--- run_pubmed_indexer.sh
+++ run_pubmed_indexer.sh
@@ -2,3 +2,4 @@ SCRIPT_DIR=/home/rgdpub/Scripts/scripts_solr8
 #$SCRIPT_DIR/aws/update_pubmed_output.sh
 #echo "Pubmed JSON output generated"
 $SCRIPT_DIR/run_indexer.sh pubmed_output OntoMate_sec pubmed
+$SCRIPT_DIR/swap_cores.sh
